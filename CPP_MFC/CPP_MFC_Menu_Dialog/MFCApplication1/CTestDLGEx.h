66712870f29438e44d932936cd1d67ccb6e8f9af
JiKang1991 <77141010+JiKang1991@users.noreply.github.com>
2021-03-15 18:20:59 +0900

    210315 studyCPP_MFC

---
+++ CPP_MFC/CPP_MFC_Menu_Dialog/MFCApplication1/CTestDLGEx.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+
+// CTestDLGEx 대화 상자
+
+class CTestDLGEx : public CDialogEx
+{
+	DECLARE_DYNAMIC(CTestDLGEx)
+
+public:
+	CTestDLGEx(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~CTestDLGEx();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DLG_Test11 };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
---
+++ CPP_MFC/CPP_MFC_Menu_Dialog/MFCApplication1/CTestDlg.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+
+// CTestDlg 대화 상자
+
+class CTestDlg : public CDialogEx
+{
+	DECLARE_DYNAMIC(CTestDlg)
+
+public:
+	CTestDlg(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~CTestDlg();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DLG_Test1 };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+	
+	DECLARE_MESSAGE_MAP()
+public:
+	virtual BOOL OnInitDialog();
+	CStatic controlStaticTest;
+	
+	afx_msg void OnBnClickedButton1();
+	CEdit controlEditInput;
+	CEdit controlEditUpper;
+	CEdit controlEditLength;
+};
