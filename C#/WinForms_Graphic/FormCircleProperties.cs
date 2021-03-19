17f529da2766dfbdffa6aca31ebba7c4cc7bb867
JiKang1991 <77141010+JiKang1991@users.noreply.github.com>
2021-03-19 17:56:27 +0900

    210319 study c# WinForm - graphic
    - graphic

---
+++ C#/WinForms_Graphic/FormCircleProperties.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms_Graphic
+{
+    public partial class FormCircleProperties : System.Windows.Forms.Form
+    {
+        public int circleHeight;
+        public int circleWidth;
+        public Color lineColor;
+        public string lineColorName;
+        public int lineSize;
+
+        public FormCircleProperties(int circleHeight, int circleWidth, Color lineColor, int lineSize)
+        {
+            InitializeComponent();
+            this.circleHeight = circleHeight;
+            tbCircleHeight.Text = circleHeight.ToString();
+            this.circleWidth = circleWidth;
+            tbCircleWidth.Text = circleWidth.ToString();
+            this.lineColor = lineColor;
+            tbCircleLineColor.Text = lineColor.Name;
+            this.lineSize = lineSize;
+            tbCircleLineSize.Text = lineSize.ToString();
+        }
+            
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if (tbCircleHeight.Text == null || tbCircleLineColor.Text == null
+                || tbCircleLineSize.Text == null || tbCircleLineColor.Text == null)
+            {
+                MessageBox.Show("비어있는 항목을 채워주세요!");
+            }
+            circleHeight = int.Parse(tbCircleHeight.Text);
+            circleWidth = int.Parse(tbCircleWidth.Text);
+            lineSize = int.Parse(tbCircleLineSize.Text);
+        }
+
+        private void tbCircleLineColor_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = colorDialog1.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+            {
+                lineColor = colorDialog1.Color;
+                tbCircleLineColor.Text = lineColor.Name;
+            }
+        
+        }
+
+    }
+}
---
+++ C#/WinForms_Graphic/FormDrawingBook.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms_Graphic
+{
+    public partial class DrawingBook : System.Windows.Forms.Form
+    {
+        Graphics graphics;
+        int circleHeight = 50;
+        int circleWeith = 50;
+        Pen pen = new Pen(Color.Black, 10);
+
+        public DrawingBook()
+        {
+            InitializeComponent();
+            graphics = canvasDraw.CreateGraphics();                
+        }
+
+        private void canvasDraw_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void canvasDraw_MouseDown(object sender, MouseEventArgs e)
+        {   
+            if (e.Button == MouseButtons.Left)
+            {                   
+                graphics.DrawEllipse(pen, e.X - circleHeight/2, e.Y - circleWeith/2, 200, 200);
+            }
+        }
+
+        private void canvasDraw_Resize(object sender, EventArgs e)
+        {
+            graphics = canvasDraw.CreateGraphics();
+        }
+
+        private void mnuEraseAll_Click(object sender, EventArgs e)
+        {
+            graphics.Clear(canvasDraw.BackColor);
+        }
+
+        private void mnuForm_Click(object sender, EventArgs e)
+        {
+            FormCircleProperties circleProperties = new FormCircleProperties(circleHeight, circleWeith, Color.Black, 10);
+            circleProperties.ShowDialog();
+
+            circleHeight = circleProperties.circleHeight;
+        }
+    }
+}
