daa3d256b72733da1ace7162346737795e9236f3
JiKang1991 <77141010+JiKang1991@users.noreply.github.com>
2021-04-06 17:38:52 +0900

    210405 study c# WinForms ChattingBasic

---
+++ C#/Winforms_NetworkProgramming/FormChattingBasicClient.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForms_Chatting_Basic
+{
+    public partial class FormChattingBasicClient : Form
+    {
+        public FormChattingBasicClient()
+        {
+            InitializeComponent();
+        }
+
+        // packet = 한 번에 전송하는 메시지 단위 
+        // 1 packet을 전송하는 메소드 입니다.
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            // Socket을 생성합니다. ( 소켓은 주소를 가지고 있지 않습니다. )
+            // TCP : 수신을 확인합니다.     UDP : 수신을 확인하지 않습니다.( ex. 방송 )
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            // Socket을 Open(Connection 수립)합니다. ( 소켓에 주소를 부여합니다. )
+            socket.Connect(tbIP.Text, int.Parse(tbPort.Text));
+
+            // 데이터를 전송합니다. ( 문자열 외에 이미지나 동영상도 가능합니다. 단, 양측간 약속된 규약(프로토콜)에 의거해야 합니다.)
+            // string자료형으로는 전송이 불가하브로 byte 배열로 encoding하여 전송합니다.
+            socket.Send(Encoding.Default.GetBytes(tbClient.Text));
+            sbClient.Text = "succeed";
+            socket.Close();
+        }
+    }
+}
---
+++ C#/Winforms_NetworkProgramming/FormChattingBasicServer.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Winforms_Chatting_Basic_Server
+{
+    public partial class FormChattingBasicServer : Form
+    {
+        public static FormChattingBasicServer mainForm;
+        public FormChattingBasicServer()
+        {
+            InitializeComponent();
+            mainForm = this; 
+        }
+        // Server
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (thread == null) {
+                thread = new Thread(serverProcess);
+                thread.Start();
+            }
+            else
+            {
+                thread.Resume();
+            }
+            ThreadState threadState = thread.ThreadState;
+            sbServerMessage.Text = threadState.ToString();
+            timer1.Enabled = true;
+        }
+
+        Thread thread = null;
+        string MainMsg; // tbServer.Text , invoke와 delegate를 사용하지 않기위한 편법
+        // Client가 Socket으로 전송하는 정보를 수신하기위해 TcpListener를 생성합니다.
+        TcpListener listener = null;
+        byte[] bArr = new byte[1024];
+
+        private void serverProcess()
+        {
+            while(true)
+            {
+                if(listener == null)
+                {
+                    listener = new TcpListener(int.Parse(tbServerPort.Text));                    
+                }
+                // 수신을 대기합니다. Stop()메소드가 호출될 때 까지 지속 대기합니다.
+                listener.Start();
+
+                // 논블로킹(nonbloking) : 해당하는 요청이 들어오지 않은 상태에서도 다른 작업을 수행하도록 합니다.
+                // pending : listener에 대기중인 요청이 있는지 확인합니다.
+                // 요청이 있는 경우에만 블록 내부의 작업을 수행합니다.
+                //if (listener.Pending())
+                {
+                    // 외부에서의 접속 요청을 수신합니다.
+                    // Socket socket = listener.AcceptSocket();
+                    // 블로킹(blocking) : 해당하는 요청이 들어오기 전까지 대기만 합니다.
+                    TcpClient tcpClient = listener.AcceptTcpClient();
+                    sbServerMessage.Text = "Connected";
+                    sbServerLabel2.Text = "Connected";
+                    NetworkStream networkStream = tcpClient.GetStream();
+                    while (networkStream.DataAvailable)
+                    {
+                        networkStream.Read(bArr, 0, 1024);
+                        string str = Encoding.Default.GetString(bArr);
+                        MainMsg += str.TrimEnd('\0');
+                    }
+                }
+                    
+                listener.Stop();
+            }
+        }        
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           tbServer.Text = MainMsg;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            thread.Suspend();
+            ThreadState threadState = thread.ThreadState;
+            sbServerMessage.Text = threadState.ToString();
+        }
+
+        private void FormChattingBasicServer_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            thread.Abort();
+            ThreadState threadState = thread.ThreadState;
+            sbServerMessage.Text = threadState.ToString();
+        }
+    }
+}
