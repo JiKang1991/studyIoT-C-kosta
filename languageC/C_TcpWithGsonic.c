8aed2e6edece1336c2d2a25b904971e7b42bd2d1
JiKang1991 <77141010+JiKang1991@users.noreply.github.com>
2021-05-26 17:45:08 +0900

    210526 study socket TCP networking for Linux with thread
    - study using thread

--- languageC/C_TcpWithGsonic.c
+++ languageC/C_TcpWithGsonic.c
@@ -8,21 +8,28 @@
 #include <unistd.h>		// close()
 
 #include <wiringPi.h>
+#include <pthread.h>
 
 char* IP = "192.168.0.29";
 int PORT = 9001;
 
+int socketHandle;
+
 double getDistance(int trigPinNum, int echoPinNum);
+void* readProcess();
 
 int main(){
 
 	int trigPinNum = 15;
 	int echoPinNum = 16;
 	
-	int socketHandle;
+	
 	struct sockaddr_in sockInfo;
 	char buf[1024];
 	int i, j, k;
+	
+	// Linux C에서는 thread 자료형의 선언을 pthread_t
+	pthread_t readThread;
 	
 	wiringPiSetup();
 	pinMode(trigPinNum, OUTPUT);
@@ -38,21 +45,21 @@ int main(){
 	k =fcntl(socketHandle, F_SETFL, 0);
 	fcntl(socketHandle, F_SETFL, k | O_NONBLOCK);
 	
+	pthread_create(&readThread, NULL, readProcess, NULL);
+	
 	while(1){
 		double distance = getDistance(trigPinNum, echoPinNum);
 		sprintf(buf, "%f\n", distance);
-		//printf("Distance : %f\n", distance);
-		//buf = Encoding.Default.getbyte(distance);
-		
+				
 		send(socketHandle, buf, strlen(buf), 0);
-		i = recv(socketHandle, buf, sizeof(buf), 0);
+		
 		// printf() : console 출력
 		// fprintf() : file 출력
-		// sprintf() : 버퍼 출력 - 문자열의 마지막에 NULL 여부에 대한 체크가 필요합니다.
-		// sprintf(buf, "%s\n", buf);
+		// sprintf() : 버퍼 출력 - 문자열의 마지막에 NULL 여부에 대한 체크가 필요합니다.		
 		
 		delay(2000);
 	}
+	pthread_join(readThread, NULL);
 	close(socketHandle);
 }
 
@@ -74,4 +81,19 @@ double getDistance(int trigPinNum, int echoPinNum){
 	double distance = (end - start) * 0.17;
 	
 	return distance;
+}
+
+// 쓰레드의 기능을 정의하는 메서드는 리턴값으로 void포인터를 갖도록 약속되어있습니다.
+void* readProcess(){
+	int i;
+	char buf[1024];
+	while(1){
+		i = recv(socketHandle, buf, sizeof(buf), 0);
+		if(i > 0){
+			buf[i] = 0;
+			printf("%s\n", buf);
+		}
+		delay(2000);
+	}
+	return NULL;
 }
